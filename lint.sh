d0c2526a80b91f4301ee9f82d43f87d2badf664c
Samuel <samuelsimweixuan@gmail.com>
2022-10-10 20:28:03 +0800

    Update with mypy package

--- lint.sh
+++ lint.sh
@@ -2,6 +2,6 @@
 echo "--------- Formatter with black ---------"
 black ~/OneDrive/Desktop/DS_Project/src/ --line-length=88 --preview
 echo "--------- Linting with flake8 ---------"
-flake8 ~/OneDrive/Desktop/DS_Project/src/ --max-length=88
+flake8 ~/OneDrive/Desktop/DS_Project/src/ --max-line-length=88
 echo "--------- Type checking with mypy ---------"
 mypy ~/OneDrive/Desktop/DS_Project/src/
